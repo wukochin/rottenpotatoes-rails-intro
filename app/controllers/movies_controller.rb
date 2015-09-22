9fe789ec3524773b41ac4fc96110b2638e9136fd
Ko-Ching Wu <wukochin@tamu.edu>
2015-09-22 16:37:59 -0500

    hw2 complete

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -11,7 +11,40 @@ class MoviesController < ApplicationController
   end
 
   def index
-    @movies = Movie.all
+
+    #pack = Hash.new
+    #pack[:by] = params[:by]
+    #pack[:ratings] = params[:ratings]
+    @by = params[:by]
+    @ratings = params[:ratings]
+    
+    # remembered user settings
+    by_remembered = @by.blank? && session[:by].present?
+    ratings_remembered = @ratings.blank? && session[:ratings].present?
+    if by_remembered || ratings_remembered
+        @ratings = session[:ratings] if ratings_remembered
+        @by = session[:by] if by_remembered
+        # RESTful URL
+        flash.keep
+        redirect_to :ratings => @ratings, :by => @by and return
+    end
+
+    # highlight each title
+    @title_header = 'hilite' if params[:by] == 'title'
+    @release_date_header = 'hilite' if params[:by] == 'release_date'
+
+    @all_ratings = Movie.ratings
+    @ratings ||= Hash[ @all_ratings.map { |d| [d, 1] }]
+    @movies = Movie.order(@by).where( :rating => @ratings.keys)
+    #@movies = Movie.order(@by).find_all_by_rating(@ratings.keys)
+    #@movies = Movie.all(order: params[:by])
+    #@movies = Movie.find_all_by_rating(@ratings.keys, :order => @by)
+    #@movies = Movie.where( :rating => @ratings.keys, :order => @by)
+    #@movies = Movie.all
+    
+    # save sessions
+    session[:by] = @by
+    session[:ratings] = @ratings
   end
 
   def new
--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,2 +1,6 @@
 class Movie < ActiveRecord::Base
+  def self.ratings 
+    ['G','PG','PG-13','R']
+  	#self.uniq.order(:rating).pluck(:rating)
+  end
 end
